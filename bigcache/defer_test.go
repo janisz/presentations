950be1b52efe29f9617d47b743facf5b83596b78
Tomasz Janiszewski <janiszt@gmail.com>
2020-01-14 12:29:36 +0100

    .

--- bigcache/defer_test.go
+++ bigcache/defer_test.go
@@ -2,22 +2,30 @@ package bigcache
 
 import "testing"
 
+var sink = 0
+
 func BenchmarkNotDefered(b *testing.B) {
         for n := 0; n < b.N; n++ {
                 notDefered()
         }
+	b.Log(sink)
 }
 
 func BenchmarkDefered(b *testing.B) {
         for n := 0; n < b.N; n++ {
                 defered()
         }
+	b.Log(sink)
 }
 
 func defered() {
-    defer func() {}()
+    defer func() {
+	    sink++
+    }()
 }
 
 func notDefered() {
-    func() {}()
+    func() {
+	    sink++
+    }()
 }
